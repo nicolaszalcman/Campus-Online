a7886f399c75d0f26c0da4006b5e7f38ab9f3df0
41916950 <41916950@A-CVI-01>
2016-10-21 12:10:52 -0300

    todo

--- Campus virtual/Models/AlumnoDivision.cs
+++ Campus virtual/Models/AlumnoDivision.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using MySql.Data.MySqlClient;
 
 namespace Campus_virtual.Models
 {
@@ -19,6 +20,36 @@ namespace Campus_virtual.Models
         public string NombreDivision { get; set; }
 
         public string DivisionCompleta { get; set; }
+
+        public AlumnoDivision TraerUnAlumno(int Id)
+        {
+            AbrirConexion abrirconexion = new AbrirConexion();
+            MySqlConnection conn = new MySqlConnection();
+            conn = abrirconexion.Conexion();
+
+            string sql = "SELECT *  FROM alumno where IdAlumno=@Id ";
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            cmd.Parameters.AddWithValue("@Id", Id);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+            AlumnoDivision unAlumno = new AlumnoDivision();
+            while (rdr.Read())
+            {
+
+
+                unAlumno.Nombre = rdr[1].ToString();
+                unAlumno.Apellido = rdr[2].ToString();
+                unAlumno.IdDivision = Convert.ToInt32(rdr[3]);
+                unAlumno.NombreUsuario = rdr[4].ToString();
+                unAlumno.Contrasenia = rdr[5].ToString();
+
+
+
+            }
+            rdr.Close();
+
+            conn.Close();
+            return unAlumno;
+        }
 
     }
 }
